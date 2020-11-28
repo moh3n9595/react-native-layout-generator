aa96fd4c8ae50f92ee900e8a470c6f51afd3b4da
Mohsen <mohsenando@gmail.com>
2020-11-28 20:50:54 +0330

    init ts

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * React Native Layout Generator
+ * Author: Mohsen Madani (mohsenando@gmail.com)
+ * Date: November 28, 2020
+ */
+
+import React from 'react';
+import {SafeAreaView, StatusBar} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView />
+    </>
+  );
+};
+
+export default App;
